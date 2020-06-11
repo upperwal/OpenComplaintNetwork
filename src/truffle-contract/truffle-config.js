21ba311c7cdb630c7eda44be549f84fd621e1a2f
Abhishek Upperwal <a.upperwal@gmail.com>
2020-06-11 14:00:38 +0530

    default compiler version

--- src/truffle-contract/truffle-config.js
+++ src/truffle-contract/truffle-config.js
@@ -1,4 +1,9 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  compilers: {
+    solc: {
+      version: "0.4.17"
+    }
+  }
 };
